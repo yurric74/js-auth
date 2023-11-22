0c8b378d1e00dde7acd5b61077c39a2dc295826d
unknown <yurric@ukr.net>
2023-11-22 11:00:01 +0200

    js-auth-2

--- src/class/user.js
+++ src/class/user.js
@@ -30,7 +30,16 @@ class User {
   static create(data) {
     const user = new User(data)
 
+    console.log(user)
     this.#list.push(user)
+
+    console.log(this.#list)
+  }
+  static getByEmail(email) {
+    return (
+      this.#list.find((user) => user.email === email) ||
+      null
+    )
   }
 }
 
--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -45,7 +51,31 @@ router.get('/signup', function (req, res) {
       ],
     },
   })
+
   // ↑↑ сюди вводимо JSON дані
+})
+
+//Этот эндпоинт регистрирует пользователя
+router.post('signup', function (res, req) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.statusCode(400).json({
+      message: 'Помилкаю Обовязкові поля відсутні',
+    })
+  }
+  try {
+    User.create({ email, password, role })
+    return res.statusCode(200).json({
+      message: 'Користувач успішно зареєстрований',
+    })
+  } catch (err) {
+    return res.statusCode(400).json({
+      message: 'Помилкаю створення користувача',
+    })
+  }
 })
 
 // Підключаємо роутер до бек-енду
